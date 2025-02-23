a8eca98e6829d829382d6e0bb4773d5b0bb06c85
Joo-Veloper <psjoo3515@gmail.com>
2025-02-23 19:51:07 +0900

    feat: Implementing a chatable member list screen

--- src/main.js
+++ src/main.js
@@ -3,9 +3,23 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "@/router/index.js";
 import "@/css/styles.css";
-// import axios from "axios";
+import axios from "axios";
 
 const app = createApp(App);
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    //인터셉트 무시, 본래 화면 라우팅
+    return Promise.reject(error);
+  }
+);
 
 app.use(router);
 app.use(vuetify);
